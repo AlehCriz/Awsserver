3a05fb22a3c6bf5a23e32387126c41ee1b324b17
AlehCriz <114173465+AlehCriz@users.noreply.github.com>
2024-07-25 07:35:17 -0400

    Update server.js
    s

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const http = require('http');
-const { format } = require('rut.js'); // Importa la función format de rut.js
 
-const PORT = 80; // Puerto 80 para HTTP
-const PORT_SSL = 443; // Puerto 443 para HTTPS
+const PORT = 80;
 
 const personas = [
   { rut: '18972631-7', nombres: 'Juan', apellidos: 'Pérez' },
   // Agrega más personas aquí...
 ];
 
+const generarTabla = () => {
+  let tabla = '<table>';
+  tabla += '<tr><th>RUT</th><th>Nombres</th><th>Apellidos</th></tr>';
+
+  personas.forEach((persona) => {
+    tabla += `<tr><td>${persona.rut}</td><td>${persona.nombres}</td><td>${persona.apellidos}</td></tr>`;
+  });
+
+  tabla += '</table>';
+  return tabla;
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Listado de Personas</h1>');
-  res.write('<ul>');
-
-  personas.forEach((persona) => {
-    const rutFormateado = format(persona.rut); // Formatea el RUT
-    res.write(`<li>${rutFormateado} - ${persona.nombres} ${persona.apellidos}</li>`);
-  });
-
-  res.write('</ul>');
+  res.write(generarTabla());
   res.end();
 });
 
